5472e19ecbcd330179375061349e89296decf099
Gugimor95 <gugimor95@gmail.com>
2021-06-25 09:50:30 +0300

    9 лаба загрузил в корень

---
+++ skills.js
@@ -0,0 +1,31 @@
+const skills = [
+    { css: "skill_zzz", skillName: "Бодрость", skillLevel: 10 },
+    { css: "skill_---", skillName: "Работоспособность", skillLevel: 70 },
+    { css: "skill_rab", skillName: "Меткость", skillLevel: 80 },
+
+];
+
+
+function addSkills() {
+    const dl = document.createElement('dl');
+    dl.classList.add('skills-list');
+    skills.forEach(skill => {
+        const dt = document.createElement('dt');
+        dt.textContent = skill.skillName;
+        dt.classList.add(skill.css);
+
+        const dd = document.createElement('dd');
+        dd.classList.add('level');
+
+        const div = document.createElement('div');
+        div.style.width = skill.skillLevel + '%';
+        div.textContent = skill.skillLevel;
+        
+        dd.appendChild(div);
+        dl.append(dt);
+        dl.append(dd);
+    });
+    document.querySelector('.skills').append(dl);
+}
+
+addSkills();

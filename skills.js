5e8c4e68c89a17de55b132bf8d83902969ce96e8
Gugimor95 <gugimor95@gmail.com>
2021-06-25 14:59:12 +0300

    мини правочка js файла

--- skills.js
+++ skills.js
@@ -1,24 +1,31 @@
-fetch('db/skills.json')
-    .then(data => data.json())
-    .then(skills => {
-        const dl = document.createElement('dl');
-        dl.classList.add('skills-list');
-        skills.forEach(skill => {
-            const dt = document.createElement('dt');
-            dt.textContent = skill.skillName;
-            dt.classList.add(skill.css);
+const skills = [
+    { css: "skill_zzz", skillName: "Бодрость", skillLevel: 10 },
+    { css: "skill_rab", skillName: "Работоспособность", skillLevel: 70 },
+    { css: "skill_---", skillName: "Меткость", skillLevel: 80 },
 
-            const dd = document.createElement('dd');
-            dd.classList.add('level');
+];
 
-            const div = document.createElement('div');
-            div.style.width = skill.skillLevel + '%';
-            div.textContent = skill.skillLevel;
 
-            dd.appendChild(div);
-            dl.append(dt);
-            dl.append(dd);
-        });
-        document.querySelector('.skills').append(dl);
-    })
-    .catch(() => console.error("Габелла!"));
+function addSkills() {
+    const dl = document.createElement('dl');
+    dl.classList.add('skills-list');
+    skills.forEach(skill => {
+        const dt = document.createElement('dt');
+        dt.textContent = skill.skillName;
+        dt.classList.add(skill.css);
+
+        const dd = document.createElement('dd');
+        dd.classList.add('level');
+
+        const div = document.createElement('div');
+        div.style.width = skill.skillLevel + '%';
+        div.textContent = skill.skillLevel;
+        
+        dd.appendChild(div);
+        dl.append(dt);
+        dl.append(dd);
+    });
+    document.querySelector('.skills').append(dl);
+}
+
+addSkills();
